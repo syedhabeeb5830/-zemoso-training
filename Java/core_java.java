da560557f2f60747c077b2409b7c194628a69dc6
syedhabeeb5830 <syedhabeeb5830@gmail.com>
2022-04-14 03:10:13 +0530

    added the corejavafile

---
+++ Java/core_java.java
@@ -0,0 +1,225 @@
+package com.company;
+
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static boolean checkStringForAllTheLetters(String str) {
+        int index = 0;
+        boolean[] vis = new boolean[26];
+
+        for (int i = 0; i < str.length(); i++) {
+            if ('a' <= str.charAt(i) && str.charAt(i) <= 'z') {
+                index = str.charAt(i) - 'a';
+            }
+            else if ('A' <= str.charAt(i) && str.charAt(i) <= 'Z') {
+                index = str.charAt(i) - 'A';
+            }
+            vis[index] = true;
+        }
+
+        for(boolean ele: vis){
+            if(!ele)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        // write your code here
+
+      /*  // Arithmetic operators
+
+
+        // declare variables
+        int a = 12, b = 5;
+
+        // addition operator
+        System.out.println("a + b = " + (a + b));
+
+        // subtraction operator
+        System.out.println("a - b = " + (a - b));
+
+        // multiplication operator
+        System.out.println("a * b = " + (a * b));
+
+        // division operator
+        System.out.println("a / b = " + (a / b));
+
+        // modulo operator
+        System.out.println("a % b = " + (a % b));
+        */
+
+
+        /* // Assignmet Operators
+
+
+        // create variables
+        int a = 4;
+        int b;
+        // assign value using =
+        b = a;
+        System.out.println("var using =: " + b);
+
+        // assign value using =+
+        b += a;
+        System.out.println("var using +=: " + b);
+
+        // assign value using =*
+        b *= a;
+        System.out.println("var using *=: " + b);
+
+        */
+
+       /* //Relational Operators
+        // create variables
+        int a = 7, b = 11;
+
+        // value of a and b
+        System.out.println("a is " + a + " and b is " + b);
+
+        // == operator
+        System.out.println(a == b);  // false
+
+        // != operator
+        System.out.println(a != b);  // true
+
+        // > operator
+        System.out.println(a > b);  // false
+
+        // < operator
+        System.out.println(a < b);  // true
+
+        // >= operator
+        System.out.println(a >= b);  // false
+
+        // <= operator
+        System.out.println(a <= b);  // true
+
+        */
+
+       /* // Logical Operators
+
+
+
+        // && operator
+        System.out.println((5 > 3) && (8 > 5));  // true
+        System.out.println((5 > 3) && (8 < 5));  // false
+
+        // || operator
+        System.out.println((5 < 3) || (8 > 5));  // true
+        System.out.println((5 > 3) || (8 < 5));  // true
+        System.out.println((5 < 3) || (8 < 5));  // false
+
+        // ! operator
+        System.out.println(!(5 == 3));  // true
+        System.out.println(!(5 > 3));  // false
+
+        */
+
+
+
+        /*// Arrays
+
+        // create an array
+        int[] age = {12, 4, 5, 2, 5};
+
+        // access each array elements
+        System.out.println("Accessing Elements of Array:");
+        System.out.println("First Element: " + age[0]);
+        System.out.println("Second Element: " + age[1]);
+        System.out.println("Third Element: " + age[2]);
+        System.out.println("Fourth Element: " + age[3]);
+        System.out.println("Fifth Element: " + age[4]);
+
+         */
+
+       /* //ArrayList
+        ArrayList<String> animals = new ArrayList<>();
+
+        // add elements in the arraylist
+        animals.add("Cat");
+        animals.add("Dog");
+        animals.add("Cow");
+        System.out.println("ArrayList: " + animals);
+
+        // get the element from the arraylist
+        String str = animals.get(1);
+        System.out.print("Element at index 1: " + str);
+
+        */
+
+
+       /* //Strings
+
+        // create first string
+        String first = "Java ";
+        System.out.println("First String: " + first);
+
+        // create second
+        String second = "Programming";
+        System.out.println("Second String: " + second);
+
+        // join two strings
+        String joinedString = first.concat(second);
+        System.out.println("Joined String: " + joinedString);
+
+        //Substring()
+        // Initializing String
+        String Str = new String("Welcome to geeksforgeeks");
+
+        // using substring() to extract substring
+        // returns (whiteSpace)geeksforgeeks
+
+        System.out.print("The extracted substring is : ");
+        System.out.println(Str.substring(10));
+
+        */
+        
+        
+        /*//StringBuilder
+
+
+        // create a StringBuilder object
+        // with a String pass as parameter
+        StringBuilder str
+                = new StringBuilder("AAAABBBCCCC");
+
+        // print string
+        System.out.println("String = "
+                + str.toString());
+
+        // reverse the string
+        StringBuilder reverseStr = str.reverse();
+
+        // print string
+        System.out.println("Reverse String = "
+                + reverseStr.toString());
+
+        // Append ', '(44) to the String
+        str.appendCodePoint(44);
+
+        // Print the modified String
+        System.out.println("Modified StringBuilder = "
+                + str);
+
+        // get capacity
+        int capacity = str.capacity();
+
+        // print the result
+        System.out.println("StringBuilder = " + str);
+        System.out.println("Capacity of StringBuilder = "
+                + capacity);
+                
+         */
+
+
+
+
+
+
+
+    }
+}
+
